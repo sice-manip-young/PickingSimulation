87d9fd672faa202a9af84e9adc14798177a9aa4e
yashimar <ryota.yashima.s8@dc.tohoku.ac.jp>
2022-03-16 14:45:19 +0900

    把持結果出力、p.DIRECT対応

--- simulator/panda_sim_grasp.py
+++ simulator/panda_sim_grasp.py
@@ -8,6 +8,10 @@ import numpy as np
 import math
 import random
 
+CONTINUE = 'continue'
+END = 'end'
+
+
 useNullSpace = 1
 ikSolver = 0
 pandaEndEffectorIndex = 11 #8
@@ -29,6 +33,7 @@ class PandaSim(object):
     
     #print("offset=",offset)
     flags = self.bullet_client.URDF_ENABLE_CACHED_GRAPHICS_SHAPES
+    self.board_center = board_center
     self.legos=[]
     
     # self.bullet_client.loadURDF("tray/traybox.urdf", board_center, [-0.5, -0.5, -0.5, 0.5], flags=flags)
@@ -106,26 +111,31 @@ class PandaSim(object):
     return min(self.state_t,self.state_dt*time_ratio)/(self.state_dt*time_ratio)*target_value + max(self.state_dt*time_ratio-self.state_t,0)/(self.state_dt*time_ratio)*init_value
   
   def step(self):
-    self.bullet_client.submitProfileTiming("step")
-    self.update_state()
+    status = self.update_state()
+    if status == END:
+      legoid = self.legos[0]
+      legopos = self.bullet_client.getBasePositionAndOrientation(legoid)
+      print('legopos:',legopos)
+      print('Is success:',legopos[0][1]>=self.board_center[1]+0.08)
+      return status
     
     t = self.t
     self.t += self.control_dt
-    if self.state in [1,2,3,4,7,8]:
+    if self.state in [0,1,4,5]:
       # pos = [0.2 * math.sin(1.5 * t), self.gripper_default_height, -0.6 + 0.1 * math.cos(1.5 * t)]
-      if self.state == 3:
+      if self.state == 0:
         pos = self.target_gripper_pos
         pos = [pos[0], self.gripper_default_height, pos[2]]
         self.prev_pos = pos
         self.gripper_rad = self.target_gripper_rad
-      if self.state == 4:
+      if self.state == 1:
         pos = self.target_gripper_pos
         self.prev_pos = pos
-      if self.state == 7:
+      if self.state == 4:
         gripper_height = self.gripper_default_height
         pos = [self.prev_pos[0], gripper_height, self.prev_pos[2]]
         self.prev_pos = pos
-      if self.state == 8:
+      if self.state == 5:
         pos = self.prev_pos
         diffX = pos[0]
         diffZ = pos[2] + 0.6
@@ -142,8 +152,9 @@ class PandaSim(object):
         target_pos = jointPoses[i]
         target_pos = self.fixed_target_value(target_pos, init_pos)
         self.bullet_client.setJointMotorControl2(self.panda, i, self.bullet_client.POSITION_CONTROL, target_pos,force=5 * 240.)
+        
     #target for fingers
-    if self.state in [5,6]:
+    elif self.state in [2,3]:
       if self.state==6:
           self.finger_target = 0.01
       if self.state==5:
@@ -153,8 +164,10 @@ class PandaSim(object):
         target_pos = self.finger_target
         target_pos = self.fixed_target_value(target_pos, init_pos)
         self.bullet_client.setJointMotorControl2(self.panda, i, self.bullet_client.POSITION_CONTROL, target_pos,force= 10)
-    self.bullet_client.submitProfileTiming()
-
+        
+    self.bullet_client.stepSimulation()
+    
+    return status
 
 class PandaSimAuto(PandaSim):
   def __init__(self, bullet_client, board_center):
@@ -162,7 +175,7 @@ class PandaSimAuto(PandaSim):
     self.state_t = 0
     self.state_dt = 0
     self.cur_state = 0
-    self.states=[0,3,5,4,6,7,8]
+    self.states=[0,1,2,3,4,5]
     self.state_durations=[1.5,4,1.5,4,1.5,1.5,1.5]
   
   def update_state(self):
@@ -177,7 +190,8 @@ class PandaSimAuto(PandaSim):
     if self.state_t > self.state_dt:
       self.cur_state += 1
       if self.cur_state>=len(self.states):
-        self.cur_state = 0
+        return END
       self.state_t = 0
       self.state=self.states[self.cur_state]
       #print("self.state=",self.state)
+    return CONTINUE
